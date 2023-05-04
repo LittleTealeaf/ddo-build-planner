de660a328d0df7e6f1f3cfbbe6d89e98e192db45
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-04 16:43:47 -0400

    oh?!

--- src/build/feat/macros.rs
+++ src/build/feat/macros.rs
@@ -1,24 +1,36 @@
-use crate::build::{bonus::bonuses::Bonuses, bonus::Bonus};
+use crate::build::{
+    attribute::{skill::Skill, Attribute},
+    bonus::{bonuses::Bonuses, source::Source},
+    bonus::{types::BonusType, Bonus},
+};
 
-// #[macro_export]
-// macro_rules! feats {
-//     ($name: ident, $($identifier: tt => $bonuses: expr),*) => {
-//         enum $name {
-//             $($identifier,)*
-//         }
-//         impl Bonuses for $name {
-//             fn get_bonuses(&self) -> Vec<Bonus> {
-//                 match self {
-//                     $($identifier => $bonuses,)*
-//                 }
-//             }
-//         }
-//     }
-//
-// }
-//
-// feats!(MyFeats, Test => {vec![]},);
-//
-// fn test() {
-//     MyFeats::Test.get_bonuses();
-// }
+// TODO: Maybe, we add in the "generics" at the end
+
+macro_rules! feats {
+    ($name: ident, $(($id: ident => ($bonuses: expr), $(($type: ty, $n: ident)),*)),*) => {
+        enum $name {
+            $($id($($type),*)),*
+        }
+
+        impl $crate::build::bonus::bonuses::Bonuses for $name {
+            fn get_bonuses(&self) -> Vec<$crate::build::bonus::Bonus> {
+                match self {
+                    $(Self::$id($($n),*) => $bonuses,)*
+                }
+            }
+        }
+    }
+}
+
+feats!(
+MyFeats,
+(Test => (vec![
+          Bonus::new(Attribute::Skill(skill.clone()), BonusType::Stacking, 2.0, Source::Unique(3), None)
+
+]), (Skill, skill)
+)
+);
+
+fn test() {
+    MyFeats::Test(Skill::Heal).get_bonuses();
+}
