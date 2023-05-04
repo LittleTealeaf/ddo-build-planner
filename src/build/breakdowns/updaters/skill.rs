6ae755fdf7aa24c6f00967c40af60d2b6414a121
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-04 15:31:03 -0400

    added macros to skills updaters

--- src/build/breakdowns/updaters/skill.rs
+++ src/build/breakdowns/updaters/skill.rs
@@ -4,66 +4,41 @@ use crate::build::{
 };
 
 macro_rules! bonus {
-    ($skill:expr, $attribute:expr, $value:expr) => {
+    ($skill:ident, $attribute:expr, $value:expr) => {
         Bonus::new(
             $attribute,
             BonusType::Skill,
             $value,
-            Source::Attribute(Attribute::Skill($skill)),
+            Source::Attribute(Attribute::Skill(Skill::$skill)),
             None,
         )
     };
 }
+
+macro_rules! spell_power {
+    ($skill: ident, $spellpower: ident, $value: expr) => {
+        bonus!(
+            $skill,
+            Attribute::SpellPower(SpellDamageType::$spellpower),
+            $value
+        )
+    };
+}
+
 pub fn get_skill_updates(skill: Skill, value: f32) -> Vec<Bonus> {
     match skill {
         Skill::Heal => vec![
-            bonus!(
-                Skill::Heal,
-                Attribute::SpellPower(SpellDamageType::Positive),
-                value
-            ),
-            bonus!(
-                Skill::Heal,
-                Attribute::SpellPower(SpellDamageType::Negative),
-                value
-            ),
+            spell_power!(Heal, Positive, value),
+            spell_power!(Heal, Negative, value),
         ],
-        Skill::Perform => vec![bonus!(
-            Skill::Perform,
-            Attribute::SpellPower(SpellDamageType::Sonic),
-            value
-        )],
+        Skill::Perform => vec![spell_power!(Perform, Sonic, value)],
         Skill::Spellcraft => vec![
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Acid),
-                value
-            ),
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Cold),
-                value
-            ),
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Electric),
-                value
-            ),
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Fire),
-                value
-            ),
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Force),
-                value
-            ),
-            bonus!(
-                Skill::Spellcraft,
-                Attribute::SpellPower(SpellDamageType::Light),
-                value
-            ),
+            spell_power!(Spellcraft, Acid, value),
+            spell_power!(Spellcraft, Cold, value),
+            spell_power!(Spellcraft, Electric, value),
+            spell_power!(Spellcraft, Fire, value),
+            spell_power!(Spellcraft, Force, value),
+            spell_power!(Spellcraft, Light, value),
         ],
         _ => Vec::new(),
     }
