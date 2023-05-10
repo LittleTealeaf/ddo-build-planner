d1f19adabe855fe7e3b4d480ecf7506adc37dec3
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-10 11:27:49 -0400

    Move bonus into the mod

--- src/logic/bonus/mod.rs
+++ src/logic/bonus/mod.rs
@@ -1,5 +1,3 @@
-mod bonus;
-pub use bonus::*;
 mod bonus_source;
 pub use bonus_source::*;
 mod bonus_types;
@@ -8,3 +6,53 @@ mod condition;
 pub use condition::*;
 mod traits;
 pub use traits::*;
+
+use super::attribute::Attribute;
+
+
+#[derive(PartialEq, Clone)]
+pub struct Bonus {
+    attribute: Attribute,
+    bonus_type: BonusType,
+    value: f32,
+    source: BonusSource,
+    conditions: Vec<Condition>,
+}
+
+impl Bonus {
+    pub fn new(
+        attribute: Attribute,
+        bonus_type: BonusType,
+        value: f32,
+        source: BonusSource,
+        conditions: Option<Vec<Condition>>,
+    ) -> Self {
+        Self {
+            attribute,
+            bonus_type,
+            value,
+            source,
+            conditions: conditions.unwrap_or(Vec::new()),
+        }
+    }
+
+    pub fn get_attribute(&self) -> Attribute {
+        self.attribute
+    }
+
+    pub fn get_value(&self) -> f32 {
+        self.value
+    }
+
+    pub fn get_bonus_type(&self) -> BonusType {
+        self.bonus_type
+    }
+
+    pub fn get_source(&self) -> BonusSource {
+        self.source
+    }
+
+    pub fn get_conditions(&self) -> Vec<Condition> {
+        self.conditions.clone()
+    }
+}
