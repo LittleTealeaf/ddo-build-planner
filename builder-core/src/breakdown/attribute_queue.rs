901567b6ba649f7ef912765d48540e43ec86e7da
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-14 11:41:22 -0400

    backload calculations to fetching
    Attributes are added to the queue only when the next item is fetched

--- builder-core/src/breakdown/attribute_queue.rs
+++ builder-core/src/breakdown/attribute_queue.rs
@@ -7,6 +7,7 @@ use crate::attribute::Attribute;
 pub struct AttributeQueue {
     forced: HashSet<Attribute>,
     queue: VecDeque<Attribute>,
+    buffer: Vec<Attribute>,
 }
 
 impl AttributeQueue {
@@ -14,23 +15,33 @@ impl AttributeQueue {
         Self {
             forced: HashSet::new(),
             queue: VecDeque::new(),
+            buffer: Vec::new(),
         }
     }
 
     pub fn get_next_attribute(&mut self) -> Option<(Attribute, bool)> {
+        self.buffer.iter().unique().for_each(|attribute| {
+            if !self.queue.contains(attribute) {
+                self.queue.push_back(*attribute)
+            }
+        });
+
+        self.buffer.clear();
+
         let attribute = self.queue.pop_front()?;
         let forced = self.forced.remove(&attribute);
         Some((attribute, forced))
     }
 
     pub fn insert_updates<T: Iterator<Item = Attribute>>(&mut self, attributes: T, forced: bool) {
-        attributes.unique().for_each(|attribute| {
-            if !self.queue.contains(&attribute) {
-                self.queue.push_back(attribute);
+        let mut attributes: Vec<Attribute> = attributes.collect();
+
+        if forced {
+            for attribute in attributes.iter() {
+                self.forced.insert(*attribute);
             }
-            if forced {
-                self.forced.insert(attribute);
-            }
-        })
+        }
+
+        self.buffer.append(&mut attributes);
     }
 }
