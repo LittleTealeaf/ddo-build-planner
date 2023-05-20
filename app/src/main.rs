764c0f56bae630705e329beda51f8433bb3c8e4c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-20 15:22:11 -0400

    Misc. changes

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,15 @@
-use builder_core::{bonus::{BonusSource, BonusType}, attribute::{Attribute, Ability, CasterLevel}, feat::Tome};
+use builder_core::{bonus::{BonusSource, BonusType}, attribute::{Attribute, Ability, CasterLevel}, feat::Tome, compiler::AttributeCompiler};
 use enum_map::Enum;
 
 fn main() {
     let value = BonusSource::Feat(builder_core::feat::Feat::Tome(Tome::SpellPower));
     println!("{}", value.into_usize());
+
+    let size = std::mem::size_of_val(&AttributeCompiler::new());
+
+    println!("Size of Attribute Compiler: {}", size);
+
+    let size = std::mem::size_of::<AttributeCompiler>();
+
+    println!("{}", size);
 }
