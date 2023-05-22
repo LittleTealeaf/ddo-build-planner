a9a15e615f7a7bff95de127f2d4d86a70451c169
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-22 15:56:09 -0400

    added documentation to spell_school

--- builder-core/src/attribute/sub/spell_school.rs
+++ builder-core/src/attribute/sub/spell_school.rs
@@ -1,20 +1,63 @@
-use crate::{attribute::GetCloned, simple_enum};
+use crate::attribute::GetCloned;
 
-simple_enum!(SpellSchool, "", (Abjuration "Abjuration", Conjuration "Conjuration", Divination "Divintation", Enchantment "Enchantment", Evocation "Evocation", Illusion "Illusion", Necromancy "Necromancy", Transmutation "Transmutation", All "All"));
+/// Describes the different schools that spells fall under in DDO.
+#[derive(
+    Copy, Clone, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize, Debug, enum_map::Enum,
+)]
+pub enum SpellSchool {
+    /// Abjuration Spells
+    Abjuration,
+    /// Conjuration Spells, or spells that *conjure* things.
+    Conjuration,
+    /// Divination Spells, or spells closely related to divine classes / features.
+    Divination,
+    /// Enchantment Spells, or spells that compell a target to do something.
+    Enchantment,
+    /// Evocation Spells, or spells that deal damage.
+    Evocation,
+    /// Spells that makes some form of false illusion.
+    Illusion,
+    /// Spells that deal with death.
+    Necromancy,
+    /// Spells that transmute or transform targets.
+    Transmutation,
+    /// Shortcut for all of the other spell schools.
+    All,
+}
+impl ToString for SpellSchool {
+    fn to_string(&self) -> String {
+        String::from(match self {
+            Self::Abjuration => "Abjuration",
+            Self::Conjuration => "Conjuration",
+            Self::Divination => "Divintation",
+            Self::Enchantment => "Enchantment",
+            Self::Evocation => "Evocation",
+            Self::Illusion => "Illusion",
+            Self::Necromancy => "Necromancy",
+            Self::Transmutation => "Transmutation",
+            Self::All => "All",
+        })
+    }
+}
+
+impl SpellSchool {
+    /// All SpellSchools except for [`SpellSchool::All`]
+    pub const VALUES: [SpellSchool; 8] = [
+        Self::Abjuration,
+        Self::Conjuration,
+        Self::Divination,
+        Self::Enchantment,
+        Self::Evocation,
+        Self::Illusion,
+        Self::Necromancy,
+        Self::Transmutation,
+    ];
+}
 
 impl GetCloned<SpellSchool> for SpellSchool {
     fn get_cloned(&self) -> Option<Vec<SpellSchool>> {
-        if let Self::All = self {
-            Some(vec![
-                Self::Abjuration,
-                Self::Conjuration,
-                Self::Divination,
-                Self::Enchantment,
-                Self::Evocation,
-                Self::Illusion,
-                Self::Necromancy,
-                Self::Transmutation,
-            ])
+        if matches!(self, Self::All) {
+            Some(Self::VALUES.to_vec())
         } else {
             None
         }
