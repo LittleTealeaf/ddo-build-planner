008002fe02398019f0e726dfe4fe8b015a611b6c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-22 16:38:49 -0400

    changes

--- builder-core/src/attribute/sub/caster_level.rs
+++ builder-core/src/attribute/sub/caster_level.rs
@@ -54,26 +54,3 @@ impl From<CasterLevel> for Attribute {
         Attribute::CasterLevel(value)
     }
 }
-
-simple_enum!(
-    SpellType,
-    "",
-    (
-        Arcane "Arcane", Divine "Divine"
-    )
-);
-
-impl SpellType {
-    /// Converts the spell type to each of the player classes represented by that type.
-    pub fn to_player_classes(&self) -> Option<Vec<PlayerClass>> {
-        match self {
-            Self::Arcane => Some(vec![
-                PlayerClass::Wizard,
-                PlayerClass::Sorcerer,
-                PlayerClass::Ranger,
-                PlayerClass::DarkHunter,
-            ]),
-            _ => None,
-        }
-    }
-}
