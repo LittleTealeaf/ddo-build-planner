c68ea8f9590677960b2a32efb5ef8f00e91aa30e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-22 20:00:29 -0400

    Added some more clippy warnings

--- builder-core/src/lib.rs
+++ builder-core/src/lib.rs
@@ -5,7 +5,11 @@
     clippy::unwrap_used,
     clippy::useless_let_if_seq,
     clippy::wildcard_dependencies,
-    clippy::equatable_if_let
+    clippy::equatable_if_let,
+    clippy::if_then_some_else_none,
+    clippy::if_not_else,
+    clippy::implicit_clone,
+    clippy::implicit_hasher
 )]
 
 //! The goal of this crate is to implement all of the build logic, including attributes, breakdowns, enhancements, feats, items, and more
@@ -19,7 +23,7 @@ pub mod bonus;
 /// I don't know what quite yet..
 pub mod character;
 /// Performs calculations for breakdowns.
-/// 
+///
 /// This is what takes in [`Bonus`] objects and calculates the resulting value for an
 /// [`Attribute`].
 pub mod compiler;
