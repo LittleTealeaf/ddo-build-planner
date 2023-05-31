6e34c1dd4817bb67311756766c9fc80e391f01d1
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-31 16:46:53 -0400

    Fixed clippy error

--- builder-core/src/attribute/types/immunity.rs
+++ builder-core/src/attribute/types/immunity.rs
@@ -9,12 +9,14 @@ use crate::attribute::{flags::Flag, Attribute};
 #[derive(PartialEq, Eq, Clone, Copy, Debug, Enum)]
 pub enum Immunity {
     Sleep,
+    Fear,
 }
 
 impl Display for Immunity {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Immunity::Sleep => write!(f, "Sleep"),
+            Immunity::Fear => write!(f, "Fear"),
         }
     }
 }
