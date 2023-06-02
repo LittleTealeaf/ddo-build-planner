535094739ddaea1d50c82bae0a1e873fcc14b6e3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-06-02 16:00:40 -0400

    added deprecation

--- builder-core/src/utils/as_string.rs
+++ builder-core/src/utils/as_string.rs
@@ -1,4 +1,5 @@
 /// A workaround for implementing `to_string` like methods for custom types.
+#[deprecated = "Implement Display for a struct instead"]
 pub trait AsString {
     /// Returns the string representation of the item.
     fn as_string(&self) -> String;
