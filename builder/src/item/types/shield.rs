ec6f0b8a3cbe0e442922f4ae3b94aeaf58bda9e2
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-06-11 15:12:23 -0400

    added PRR bonus from shields

--- builder/src/item/types/shield.rs
+++ builder/src/item/types/shield.rs
@@ -2,6 +2,15 @@ use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
+use crate::{
+    attribute::{
+        flags::{Flag, OffHandType},
+        types::Sheltering,
+        Attribute, GetBonuses,
+    },
+    bonus::{Bonus, BonusType},
+};
+
 /// The types of shields.
 #[cfg_attr(test, derive(enum_map::Enum))]
 #[derive(PartialEq, Eq, Clone, Copy, Debug, PartialOrd, Ord, Serialize, Deserialize)]
@@ -29,5 +38,34 @@ impl Display for ShieldType {
             ShieldType::TowerShield => write!(f, "Tower Shield"),
             ShieldType::Orb => write!(f, "Orb"),
         }
+    }
+}
+
+impl GetBonuses for ShieldType {
+    fn get_bonuses(&self, value: f32) -> Option<Vec<crate::bonus::Bonus>> {
+        (value > 0f32).then(|| match self {
+            ShieldType::SmallShield => Some(vec![Bonus::new(
+                Sheltering::Physical.into(),
+                BonusType::Stacking,
+                5f32.into(),
+                Attribute::from(Flag::from(OffHandType::from(ShieldType::SmallShield))).into(),
+                None,
+            )]),
+            ShieldType::LargeShield => Some(vec![Bonus::new(
+                Sheltering::Physical.into(),
+                BonusType::Stacking,
+                10f32.into(),
+                Attribute::from(Flag::from(OffHandType::from(ShieldType::LargeShield))).into(),
+                None,
+            )]),
+            ShieldType::TowerShield => Some(vec![Bonus::new(
+                Sheltering::Physical.into(),
+                BonusType::Stacking,
+                15f32.into(),
+                Attribute::from(Flag::from(OffHandType::from(ShieldType::TowerShield))).into(),
+                None,
+            )]),
+            _ => None,
+        })?
     }
 }
