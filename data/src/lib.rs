8ad24a0a60ddd933112d21522cc656f08e64633b
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-06-16 07:47:12 -0400

    Added clippy warnings to data

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![warn(
+    missing_docs,
+    clippy::inefficient_to_string,
+    clippy::unwrap_used,
+    clippy::useless_let_if_seq,
+    clippy::wildcard_dependencies,
+    clippy::equatable_if_let,
+    clippy::if_then_some_else_none,
+    clippy::if_not_else,
+    clippy::implicit_clone,
+    clippy::implicit_hasher,
+    clippy::dbg_macro,
+    clippy::default_trait_access,
+    clippy::empty_line_after_outer_attr,
+    clippy::explicit_iter_loop,
+    clippy::explicit_into_iter_loop,
+    clippy::get_unwrap,
+    clippy::large_types_passed_by_value,
+    clippy::manual_ok_or,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc
+)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! This crate contains large datasets such as items.
