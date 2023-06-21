c4c2f2002cd968be69bf4319ef30c6652e13bc6e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-06-21 17:59:55 -0400

    feat(data): Implement bundling resource files (#107)

--- data/build.rs
+++ data/build.rs
@@ -1,7 +1,26 @@
-use std::env;
+use std::{env, fs::File, io::Write, path::Path};
+
+use errors::Error;
+use serde::Serialize;
 
 fn main() -> Result<(), self::errors::Error> {
-    let _out_dir = env::var("OUT_DIR")?;
+    write_artifact(
+        "test",
+        String::from("This is test data from the build script"),
+    )?;
+
+    Ok(())
+}
+
+fn write_artifact<T>(name: &str, item: T) -> Result<(), Error>
+where
+    T: Serialize,
+{
+    let path = Path::new(&env::var("OUT_DIR")?).join(name);
+
+    let mut file = File::create(path)?;
+
+    file.write_all(ron::to_string(&item)?.as_bytes())?;
 
     Ok(())
 }
@@ -11,12 +30,26 @@ mod errors {
 
     #[derive(Debug)]
     pub enum Error {
-        VarError(VarError),
+        Environment(VarError),
+        Serialize(ron::Error),
+        IO(std::io::Error),
     }
 
     impl From<VarError> for Error {
         fn from(value: VarError) -> Self {
-            Self::VarError(value)
+            Self::Environment(value)
+        }
+    }
+
+    impl From<ron::error::Error> for Error {
+        fn from(value: ron::Error) -> Self {
+            Self::Serialize(value)
+        }
+    }
+
+    impl From<std::io::Error> for Error {
+        fn from(value: std::io::Error) -> Self {
+            Self::IO(value)
         }
     }
 }
