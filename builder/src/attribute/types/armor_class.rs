0e1105dd0cae9c2e9ded7743506d3bebaa48270a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-07-04 10:52:19 -0400

    corrected max dex bonus calculations

--- builder/src/attribute/types/armor_class.rs
+++ builder/src/attribute/types/armor_class.rs
@@ -9,15 +9,13 @@ use crate::{
         Attribute, DefaultBonuses, GetBonuses,
     },
     bonus::{Bonus, BonusSource, BonusType, Condition, Value},
-    equipment::item::types::ShieldType,
+    equipment::item::types::{ArmorType, ShieldType},
 };
 
 /// Represents different attributes that relate to Armor Class
 #[cfg_attr(feature = "enum_ord", derive(enum_map::Enum))]
 #[derive(PartialEq, Eq, Clone, Copy, Debug, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum ArmorClass {
-    /// Flat bonuses to armor class
-    Bonus,
     /// Bonuses to armor class from armor
     ArmorBonus,
     /// Bonuses to armor class from shields
@@ -26,18 +24,20 @@ pub enum ArmorClass {
     ArmorScalar,
     /// Scaling for [`ArmorClass::ShieldBonus`]
     ShieldScalar,
-    /// Scaling for [`ArmorClass::Bonus`]
-    Scalar,
     /// Natural Armor
     NaturalArmor,
-    /// Calculated Max Dex Bonus
-    ///
-    /// DO NOT MANUALLY ADD BONUSES TO THIS ATTRIBUTE.
-    CalculatedMaxDexBonus,
     /// Max Dex Bonus for Armor
     ArmorMaxDexBonus,
     /// Max Dex Bonus for Tower Shield
     ShieldMaxDexBonus,
+    /// Calculated Max Dex Bonus
+    ///
+    /// DO NOT MANUALLY ADD BONUSES TO THIS ATTRIBUTE.
+    CalculatedMaxDexBonus,
+    /// Flat bonuses to armor class
+    Bonus,
+    /// Scaling for [`ArmorClass::Bonus`]
+    Scalar,
 }
 
 impl Display for ArmorClass {
@@ -92,15 +92,17 @@ impl GetBonuses for ArmorClass {
     }
 }
 
-// TODO: Impelemnt
-const IS_WEARING_ARMOR: Condition = Condition::Any(vec![]);
+fn is_wearing_armor() -> Condition {
+    Condition::Any(vec![
+        Condition::has(Flag::ArmorType(ArmorType::Light).into()),
+        Condition::has(Flag::ArmorType(ArmorType::Medium).into()),
+        Condition::has(Flag::ArmorType(ArmorType::Heavy).into()),
+    ])
+}
 
-const IS_WIELDING_SHIELD: Condition = Condition::GreaterThan(
-    Value::Attribute(Attribute::Flag(Flag::OffHandType(OffHandType::Shield(
-        ShieldType::TowerShield,
-    )))),
-    Value::Value(0f32),
-);
+fn is_wielding_tower_shield() -> Condition {
+    Condition::has(Flag::OffHandType(OffHandType::Shield(ShieldType::TowerShield)).into())
+}
 
 impl DefaultBonuses for ArmorClass {
     fn get_default_bonuses() -> Vec<Bonus> {
@@ -111,9 +113,9 @@ impl DefaultBonuses for ArmorClass {
                 Attribute::ArmorClass(ArmorClass::ArmorMaxDexBonus).into(),
                 BonusSource::Base,
                 Some(Condition::All(vec![
-                    IS_WEARING_ARMOR,
+                    is_wearing_armor(),
                     Condition::NotAll(vec![
-                        IS_WIELDING_SHIELD,
+                        is_wielding_tower_shield(),
                         Condition::GreaterThan(
                             Attribute::from(ArmorClass::ArmorMaxDexBonus).into(),
                             Attribute::from(ArmorClass::ShieldMaxDexBonus).into(),
@@ -127,9 +129,9 @@ impl DefaultBonuses for ArmorClass {
                 Attribute::ArmorClass(ArmorClass::ShieldMaxDexBonus).into(),
                 BonusSource::Base,
                 Some(Condition::All(vec![
-                    IS_WIELDING_SHIELD,
+                    is_wielding_tower_shield(),
                     Condition::NotAll(vec![
-                        IS_WEARING_ARMOR,
+                        is_wearing_armor(),
                         Condition::GreaterThan(
                             Attribute::from(ArmorClass::ShieldMaxDexBonus).into(),
                             Attribute::from(ArmorClass::ArmorMaxDexBonus).into(),
@@ -137,37 +139,28 @@ impl DefaultBonuses for ArmorClass {
                     ]),
                 ])),
             ),
-            // If max dex bonus is higher than value
+            // If there is a max dex bonus
             Bonus::new(
                 ArmorClass::Bonus.into(),
                 BonusType::AbilityModifier,
-                Attribute::Ability(Ability::Dexterity).into(),
+                Value::Min(vec![
+                    Attribute::AbilityModifier(Ability::Dexterity).into(),
+                    Attribute::ArmorClass(ArmorClass::CalculatedMaxDexBonus).into(),
+                ]),
                 BonusSource::Base,
-                Some(Condition::Any(vec![
-                    Condition::not_have(ArmorClass::CalculatedMaxDexBonus.into()),
-                    Condition::LessThan(
-                        Attribute::Ability(Ability::Dexterity).into(),
-                        Attribute::ArmorClass(ArmorClass::CalculatedMaxDexBonus).into(),
-                    ),
-                    Condition::EqualTo(
-                        Attribute::Ability(Ability::Dexterity).into(),
-                        Attribute::ArmorClass(ArmorClass::CalculatedMaxDexBonus).into(),
-                    ),
-                ])),
+                Some(Condition::has(Attribute::ArmorClass(
+                    ArmorClass::CalculatedMaxDexBonus,
+                ))),
             ),
-            // If max dex bonus is lower than value
+            // If there is not a max dex bonus
             Bonus::new(
                 ArmorClass::Bonus.into(),
                 BonusType::AbilityModifier,
-                Attribute::ArmorClass(ArmorClass::CalculatedMaxDexBonus).into(),
+                Attribute::AbilityModifier(Ability::Dexterity).into(),
                 BonusSource::Base,
-                Some(Condition::All(vec![
-                    Condition::has(ArmorClass::CalculatedMaxDexBonus.into()),
-                    Condition::GreaterThan(
-                        Attribute::Ability(Ability::Dexterity).into(),
-                        Attribute::ArmorClass(ArmorClass::CalculatedMaxDexBonus).into(),
-                    ),
-                ])),
+                Some(Condition::not_have(
+                    ArmorClass::CalculatedMaxDexBonus.into(),
+                )),
             ),
         ]
     }
