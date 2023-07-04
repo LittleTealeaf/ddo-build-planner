bd91518747d400278a406c76a9a3654c3c0503e8
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-07-04 13:58:15 -0400

    Fix OrdSet documentation

--- builder/src/attribute/traits.rs
+++ builder/src/attribute/traits.rs
@@ -128,7 +128,7 @@ pub trait AttributeDependencies {
     /// Checks if a given attribute is a dependdency of this object
     fn has_attr_dependency(&self, attribute: Attribute) -> bool;
 
-    /// Collects dependencies into an OrdSet
+    /// Collects dependencies into an `OrdSet`
     fn include_attr_dependency(&self, set: &mut OrdSet<Attribute>);
 
     /// Creates an ord set for dependencies
