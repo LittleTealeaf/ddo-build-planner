06b35f33c1c57f5e5a7a49b33afed327d8c35799
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-07-05 12:20:15 -0400

    Added tests for has_dependency

--- builder/src/bonus/value.rs
+++ builder/src/bonus/value.rs
@@ -176,4 +176,82 @@ mod tests {
             }
         });
     }
+
+    mod dependencies {
+        use super::*;
+
+        mod has_dependency {
+            use super::*;
+
+            #[test]
+            fn value() {
+                let value = Value::Value(10f32);
+
+                assert!(!value.has_attr_dependency(Attribute::Debug(0)));
+            }
+
+            #[test]
+            fn attribute() {
+                let value = Value::Attribute(Attribute::Debug(0));
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+            }
+
+            #[test]
+            fn sum() {
+                let value = Value::Sum(vec![Attribute::Debug(0).into(), 10f32.into()]);
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+            }
+
+            #[test]
+            fn product() {
+                let value = Value::Product(vec![Attribute::Debug(0).into(), 10f32.into()]);
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+            }
+
+            #[test]
+            fn min() {
+                let value = Value::Min(vec![Attribute::Debug(0).into(), 10f32.into()]);
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+            }
+
+            #[test]
+            fn max() {
+                let value = Value::Max(vec![Attribute::Debug(0).into(), Attribute::Debug(1).into()]);
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(value.has_attr_dependency(Attribute::Debug(1)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(2)));
+            }
+
+            #[test]
+            fn floor() {
+                let value = Value::Floor(Value::Attribute(Attribute::Debug(0)).into());
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+            }
+
+            #[test]
+            fn if_value() {
+                let value = Value::If(
+                    Condition::GreaterThan(Attribute::Debug(0).into(), 1f32.into()).into(),
+                    Value::from(Attribute::Debug(1)).into(),
+                    Value::from(Attribute::Debug(2)).into()
+                );
+
+                assert!(value.has_attr_dependency(Attribute::Debug(0)));
+                assert!(value.has_attr_dependency(Attribute::Debug(1)));
+                assert!(value.has_attr_dependency(Attribute::Debug(2)));
+                assert!(!value.has_attr_dependency(Attribute::Debug(3)));
+            }
+        }
+    }
 }
