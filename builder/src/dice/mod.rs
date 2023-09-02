e3607b5558027694c951d37c8efced32ff1d0a81
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-09-02 14:06:43 -0400

    added dice and formula files

--- builder/src/dice/mod.rs
+++ builder/src/dice/mod.rs
@@ -1,2 +1,8 @@
 //! Provides the ability to represent dice, including randomly rolling, giving probability, and
 //! additional functionality
+
+mod dice;
+pub use dice::*;
+
+mod formula;
+pub use formula::*;
