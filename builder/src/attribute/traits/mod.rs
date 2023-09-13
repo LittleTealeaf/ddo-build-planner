c58893c4ef9df22f8438b3c5f61746ceabe4e790
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-09-13 11:41:09 -0400

    Use macro instead of manual

--- builder/src/attribute/traits/mod.rs
+++ builder/src/attribute/traits/mod.rs
@@ -1,10 +1,7 @@
-mod get_bonuses;
-pub use get_bonuses::*;
-mod track_attribute;
-pub use track_attribute::*;
-
-mod default_bonuses;
-pub use default_bonuses::*;
-
-mod attribute_dependencies;
-pub use attribute_dependencies::*;
+use utils::public_modules;
+public_modules!(
+    attribute_dependencies,
+    default_bonuses,
+    get_bonuses,
+    track_attribute
+);
