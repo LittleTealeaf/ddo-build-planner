b80ad649302af89462bc17e07c0ca273f0a712b3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-10-21 13:00:36 -0400

    refactor value tests

--- builder/tests/bonus.rs
+++ builder/tests/bonus.rs
@@ -1,197 +1,174 @@
-mod value {
-    use builder::{attribute::Attribute, bonus::Value};
+use builder::{
+    attribute::{Attribute, AttributeDependencies},
+    bonus::{Condition, Value},
+};
 
-    #[test]
-    fn from_attribute() {
-        let value = Value::from(Attribute::Debug(4));
-        assert_eq!(value, Value::Attribute(Attribute::Debug(4)));
-    }
-
-    #[test]
-    fn from_value() {
-        let value = Value::from(3f32);
-
-        assert!({
-            if let Value::Value(val) = value {
-                val == 3f32
-            } else {
-                false
-            }
-        });
-    }
-
-    mod dependencies {
+mod has_dependency {
+    use super::*;
+    mod value {
         use super::*;
 
-        mod has_dependency {
-            use builder::{attribute::AttributeDependencies, bonus::Condition};
+        #[test]
+        fn value() {
+            let value = Value::Value(10f32);
 
-            use super::*;
-
-            #[test]
-            fn value() {
-                let value = Value::Value(10f32);
-
-                assert!(!value.has_attr_dependency(Attribute::Debug(0)));
-            }
-
-            #[test]
-            fn attribute() {
-                let value = Value::Attribute(Attribute::Debug(0));
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
-            }
-
-            #[test]
-            fn sum() {
-                let value = Value::Sum(vec![Attribute::Debug(0).into(), 10f32.into()]);
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
-            }
-
-            #[test]
-            fn product() {
-                let value = Value::Product(vec![Attribute::Debug(0).into(), 10f32.into()]);
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
-            }
-
-            #[test]
-            fn min() {
-                let value = Value::Min(vec![Attribute::Debug(0).into(), 10f32.into()]);
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
-            }
-
-            #[test]
-            fn max() {
-                let value =
-                    Value::Max(vec![Attribute::Debug(0).into(), Attribute::Debug(1).into()]);
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(value.has_attr_dependency(Attribute::Debug(1)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(2)));
-            }
-
-            #[test]
-            fn floor() {
-                let value = Value::Floor(Value::Attribute(Attribute::Debug(0)).into());
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(1)));
-            }
-
-            #[test]
-            fn if_value() {
-                let value = Value::If {
-                    condition: Condition::GreaterThan(Attribute::Debug(0).into(), 1f32.into())
-                        .into(),
-                    if_true: Value::from(Attribute::Debug(1)).into(),
-                    if_false: Value::from(Attribute::Debug(2)).into(),
-                };
-
-                assert!(value.has_attr_dependency(Attribute::Debug(0)));
-                assert!(value.has_attr_dependency(Attribute::Debug(1)));
-                assert!(value.has_attr_dependency(Attribute::Debug(2)));
-                assert!(!value.has_attr_dependency(Attribute::Debug(3)));
-            }
+            assert!(!value.has_attr_dependency(Attribute::Debug(0)));
         }
 
-        mod include_dependencies {
-            use builder::{attribute::AttributeDependencies, bonus::Condition};
+        #[test]
+        fn attribute() {
+            let value = Value::Attribute(Attribute::Debug(0));
 
-            use super::*;
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+        }
 
-            #[test]
-            fn value() {
-                let value = Value::Value(10f32);
-                let deps = value.get_attr_dependencies();
+        #[test]
+        fn sum() {
+            let value = Value::Sum(vec![Attribute::Debug(0).into(), 10f32.into()]);
 
-                assert!(!deps.contains(&Attribute::Debug(0)));
-            }
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+        }
 
-            #[test]
-            fn attribute() {
-                let value = Value::Attribute(Attribute::Debug(0));
+        #[test]
+        fn product() {
+            let value = Value::Product(vec![Attribute::Debug(0).into(), 10f32.into()]);
 
-                let deps = value.get_attr_dependencies();
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+        }
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(!deps.contains(&Attribute::Debug(1)));
-            }
+        #[test]
+        fn min() {
+            let value = Value::Min(vec![Attribute::Debug(0).into(), 10f32.into()]);
 
-            #[test]
-            fn sum() {
-                let value = Value::Sum(vec![Attribute::Debug(0).into(), 10f32.into()]);
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+        }
 
-                let deps = value.get_attr_dependencies();
+        #[test]
+        fn max() {
+            let value = Value::Max(vec![Attribute::Debug(0).into(), Attribute::Debug(1).into()]);
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(!deps.contains(&Attribute::Debug(1)));
-            }
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(value.has_attr_dependency(Attribute::Debug(1)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(2)));
+        }
 
-            #[test]
-            fn product() {
-                let value = Value::Product(vec![Attribute::Debug(0).into(), 10f32.into()]);
+        #[test]
+        fn floor() {
+            let value = Value::Floor(Value::Attribute(Attribute::Debug(0)).into());
 
-                let deps = value.get_attr_dependencies();
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(1)));
+        }
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(!deps.contains(&Attribute::Debug(1)));
-            }
+        #[test]
+        fn if_value() {
+            let value = Value::If {
+                condition: Condition::GreaterThan(Attribute::Debug(0).into(), 1f32.into()).into(),
+                if_true: <Box<Value>>::from(Value::from(Attribute::Debug(1))),
+                if_false: Value::from(Attribute::Debug(2)).into(),
+            };
 
-            #[test]
-            fn min() {
-                let value = Value::Min(vec![Attribute::Debug(0).into(), 10f32.into()]);
+            assert!(value.has_attr_dependency(Attribute::Debug(0)));
+            assert!(value.has_attr_dependency(Attribute::Debug(1)));
+            assert!(value.has_attr_dependency(Attribute::Debug(2)));
+            assert!(!value.has_attr_dependency(Attribute::Debug(3)));
+        }
+    }
+}
 
-                let deps = value.get_attr_dependencies();
+mod include_dependencies {
+    use super::*;
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(!deps.contains(&Attribute::Debug(1)));
-            }
+    mod value {
+        use super::*;
 
-            #[test]
-            fn max() {
-                let value =
-                    Value::Max(vec![Attribute::Debug(0).into(), Attribute::Debug(1).into()]);
+        #[test]
+        fn value() {
+            let value = Value::Value(10f32);
+            let deps = value.get_attr_dependencies();
 
-                let deps = value.get_attr_dependencies();
+            assert!(!deps.contains(&Attribute::Debug(0)));
+        }
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(deps.contains(&Attribute::Debug(1)));
-                assert!(!deps.contains(&Attribute::Debug(2)));
-            }
+        #[test]
+        fn attribute() {
+            let value = Value::Attribute(Attribute::Debug(0));
 
-            #[test]
-            fn floor() {
-                let value = Value::Floor(Value::Attribute(Attribute::Debug(0)).into());
+            let deps = value.get_attr_dependencies();
 
-                let deps = value.get_attr_dependencies();
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(!deps.contains(&Attribute::Debug(1)));
+        }
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(!deps.contains(&Attribute::Debug(1)));
-            }
+        #[test]
+        fn sum() {
+            let value = Value::Sum(vec![Attribute::Debug(0).into(), 10f32.into()]);
 
-            #[test]
-            fn if_value() {
-                let value = Value::If {
-                    condition: Condition::GreaterThan(Attribute::Debug(0).into(), 1f32.into())
-                        .into(),
-                    if_true: Value::from(Attribute::Debug(1)).into(),
-                    if_false: Value::from(Attribute::Debug(2)).into(),
-                };
+            let deps = value.get_attr_dependencies();
 
-                let deps = value.get_attr_dependencies();
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(!deps.contains(&Attribute::Debug(1)));
+        }
 
-                assert!(deps.contains(&Attribute::Debug(0)));
-                assert!(deps.contains(&Attribute::Debug(1)));
-                assert!(deps.contains(&Attribute::Debug(2)));
-                assert!(!deps.contains(&Attribute::Debug(3)));
-            }
+        #[test]
+        fn product() {
+            let value = Value::Product(vec![Attribute::Debug(0).into(), 10f32.into()]);
+
+            let deps = value.get_attr_dependencies();
+
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(!deps.contains(&Attribute::Debug(1)));
+        }
+
+        #[test]
+        fn min() {
+            let value = Value::Min(vec![Attribute::Debug(0).into(), 10f32.into()]);
+
+            let deps = value.get_attr_dependencies();
+
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(!deps.contains(&Attribute::Debug(1)));
+        }
+
+        #[test]
+        fn max() {
+            let value = Value::Max(vec![Attribute::Debug(0).into(), Attribute::Debug(1).into()]);
+
+            let deps = value.get_attr_dependencies();
+
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(deps.contains(&Attribute::Debug(1)));
+            assert!(!deps.contains(&Attribute::Debug(2)));
+        }
+
+        #[test]
+        fn floor() {
+            let value = Value::Floor(Value::Attribute(Attribute::Debug(0)).into());
+
+            let deps = value.get_attr_dependencies();
+
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(!deps.contains(&Attribute::Debug(1)));
+        }
+
+        #[test]
+        fn if_value() {
+            let value = Value::If {
+                condition: Condition::GreaterThan(Attribute::Debug(0).into(), 1f32.into()).into(),
+                if_true: Value::from(Attribute::Debug(1)).into(),
+                if_false: Value::from(Attribute::Debug(2)).into(),
+            };
+
+            let deps = value.get_attr_dependencies();
+
+            assert!(deps.contains(&Attribute::Debug(0)));
+            assert!(deps.contains(&Attribute::Debug(1)));
+            assert!(deps.contains(&Attribute::Debug(2)));
+            assert!(!deps.contains(&Attribute::Debug(3)));
         }
     }
 }
