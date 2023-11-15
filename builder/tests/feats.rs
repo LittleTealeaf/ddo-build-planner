d9fa4f8a196667b60c4b94c11cdae54c43b1f614
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-15 07:52:08 -0500

    added additional tests

--- builder/tests/feats.rs
+++ builder/tests/feats.rs
@@ -7,7 +7,7 @@ use builder::{
 };
 
 #[test]
-fn simple_proficinecy_provides_proficiencies() {
+fn simple_proficiency_provides_proficiencies() {
     let mut compiler = Compiler::default();
     compiler.add_bonus(Bonus::new(
         Attribute::Feat(Feat::Proficiency(Proficiency::SimpleWeaponProficiency)),
@@ -20,6 +20,24 @@ fn simple_proficinecy_provides_proficiencies() {
     assert!(
         compiler.get_attribute(&Attribute::Feat(Feat::Proficiency(
             Proficiency::WeaponProficiency(WeaponType::Dagger)
+        ))) > 0f32
+    );
+}
+
+#[test]
+fn martial_proficiency_provides_proficiencies() {
+    let mut compiler = Compiler::default();
+    compiler.add_bonus(Bonus::new(
+        Attribute::Feat(Feat::Proficiency(Proficiency::MartialWeaponProficiency)),
+        BonusType::Stacking,
+        1f32.into(),
+        BonusSource::Debug(0),
+        None,
+    ));
+
+    assert!(
+        compiler.get_attribute(&Attribute::Feat(Feat::Proficiency(
+            Proficiency::WeaponProficiency(WeaponType::Falchion)
         ))) > 0f32
     );
 }
