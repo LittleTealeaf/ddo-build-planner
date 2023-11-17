64709965b1c0188bce64da9cb6ceaf2773f89178
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-17 10:35:14 -0500

    moved lint configuration into workspace toml

--- data/examples/resource.rs
+++ data/examples/resource.rs
@@ -1,5 +1,7 @@
+//! Demonstrates obtaining data dynamically stored at build time
 use data::get_test_data;
 
+/// Main Function
 pub fn main() {
     let data = get_test_data().unwrap();
     println!("Data returned: {}", data);
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,10 +1,3 @@
-#![warn(missing_docs, clippy::pedantic, clippy::nursery)]
-#![allow(
-    dead_code,
-    clippy::must_use_candidate,
-    clippy::too_many_lines,
-    clippy::module_name_repetitions
-)]
 //! A collection of utility functions used throughout the project.
 
 pub mod float;
