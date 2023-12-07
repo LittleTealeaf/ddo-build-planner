857f05ce1689723ee34f08e6979998fd0d9dff65
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-07 13:28:51 -0500

    redid armor class

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,23 +1,80 @@
 //! Application Starting Point
 use builder::{
     attribute::Attribute,
-    bonus::{Bonus, BonusSource, BonusType},
+    bonus::{Bonus, BonusSource, BonusType, Value, Condition},
     compiler::Compiler,
-    equipment::item::types::ShieldType,
+    equipment::item::types::{ShieldType, ArmorType},
     types::{
         ability::Ability,
         armor_class::ArmorClass,
-        flag::OffHandType,
+        flag::{OffHandType, Flag},
         player_class::PlayerClass,
         race::Race,
         weapon_attribute::{WeaponHand, WeaponStat},
     },
 };
 
+
+
+fn is_wearing_armor() -> Condition {
+    Condition::has(Flag::ArmorType(ArmorType::Light).into())
+        | Condition::has(Flag::ArmorType(ArmorType::Medium).into())
+        | Condition::has(Flag::ArmorType(ArmorType::Heavy).into())
+}
+
+fn is_wielding_tower_shield() -> Condition {
+    Condition::has(Attribute::from(Flag::OffHandType(OffHandType::Shield(
+        ShieldType::TowerShield,
+    ))))
+}
+
 fn main() {
     let mut compiler = Compiler::default();
 
     println!("Adding Bonuses");
+
+    println!(
+        "{}",
+        ron::to_string(
+            &([
+                Value::Attribute(Attribute::ArmorClass(ArmorClass::Bonus)),
+                Value::Attribute(Attribute::ArmorClass(ArmorClass::NaturalArmor)),
+                Value::Attribute(Attribute::ArmorClass(ArmorClass::ShieldBonus))
+                    * (Value::Value(1f32)
+                        + Value::Attribute(Attribute::ArmorClass(ArmorClass::ShieldScalar))),
+                Value::Attribute(Attribute::ArmorClass(ArmorClass::ArmorBonus))
+                    * (Value::Value(1f32)
+                        + Value::Attribute(Attribute::ArmorClass(ArmorClass::ArmorScalar))),
+                Value::Value(10f32),
+            Value::If {
+                    condition: is_wearing_armor().into(),
+                    if_true: Box::new(Value::If {
+                        condition: is_wielding_tower_shield().into(),
+                        if_true: Value::Attribute(Attribute::AbilityModifier(Ability::Dexterity))
+                            .min(Value::Attribute(Attribute::ArmorClass(ArmorClass::ArmorMaxDex)))
+                            .min(Value::Attribute(Attribute::ArmorClass(ArmorClass::ShieldMaxDex)))
+                            .into(),
+                        if_false: Value::Attribute(Attribute::AbilityModifier(Ability::Dexterity))
+                            .min(Value::Attribute(Attribute::ArmorClass(ArmorClass::ArmorMaxDex)))
+                            .into(),
+                    }),
+                    if_false: Box::new(Value::If {
+                        condition: is_wielding_tower_shield().into(),
+                        if_false: Value::Attribute(Attribute::AbilityModifier(Ability::Dexterity))
+                            .into(),
+                        if_true: Value::Attribute(Attribute::AbilityModifier(Ability::Dexterity))
+                            .min(Value::Attribute(Attribute::ArmorClass(ArmorClass::ShieldMaxDex)))
+                            .into(),
+                    }),
+                }
+            ]
+            .into_iter()
+            .sum::<Value>()
+                * (Value::Value(1f32)
+                    + Value::Attribute(Attribute::ArmorClass(ArmorClass::TotalScalar))))
+        )
+        .unwrap()
+    );
 
     compiler.add_bonuses(vec![Bonus::new(
         Ability::All.into(),
