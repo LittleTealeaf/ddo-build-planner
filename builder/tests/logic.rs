6a266db20f22b4e2f9450b8d85763ab4be2030d2
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-05 12:27:04 -0500

    merged logic

--- builder/tests/logic.rs
+++ builder/tests/logic.rs
@@ -497,3 +497,36 @@ mod sheltering {
         }
     }
 }
+
+mod race {
+    mod dwarf {
+        use builder::{
+            attribute::Attribute,
+            bonus::{Bonus, BonusSource},
+            breakdowns::Breakdowns,
+            feat::{Feat, Proficiency},
+            types::{item::WeaponType, race::Race},
+        };
+
+        #[test]
+        fn dwarven_war_axe() {
+            let mut breakdowns = Breakdowns::new();
+            breakdowns.insert_bonus(Bonus::flag(Race::Dwarf, BonusSource::Debug(0)));
+            assert!(
+                breakdowns.get_attribute(&Attribute::Feat(Feat::Proficiency(
+                    Proficiency::WeaponProficiency(WeaponType::DwarvenWarAxe)
+                ))) == 0.into()
+            );
+            breakdowns.insert_bonus(Bonus::feat(
+                Proficiency::MartialWeaponProficiency,
+                BonusSource::Debug(1),
+                None,
+            ));
+            assert!(
+                breakdowns.get_attribute(&Attribute::Feat(Feat::Proficiency(
+                    Proficiency::WeaponProficiency(WeaponType::DwarvenWarAxe)
+                ))) > 0.into()
+            );
+        }
+    }
+}
