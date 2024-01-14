f55d874deb32963f1f83c2773c53024c7149ce1e
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-14 09:50:52 -0500

    changes

--- app/src/main.rs
+++ app/src/main.rs
@@ -45,31 +45,4 @@ fn main() {
         "{}",
         ron::ser::to_string_pretty(&bonuses, PrettyConfig::new()).unwrap()
     );
-
-    //
-    // let mut breakdowns = Breakdowns::new();
-    //
-    // breakdowns.import_set_bonuses();
-    //
-    // breakdowns.insert_bonus(Bonus::new(
-    //     Attribute::SetBonus("Might of the Abashai".to_string()),
-    //     BonusType::Stacking,
-    //     5,
-    //     BonusSource::Custom(0),
-    //     None,
-    // ));
-    // // clean up
-    //
-    // breakdowns.insert_bonus(Bonus::new(
-    //     Ability::All,
-    //     BonusType::Stacking,
-    //     50,
-    //     BonusSource::Custom(1),
-    //     None,
-    // ));
-    //
-    // println!(
-    //     "{}",
-    //     ron::ser::to_string_pretty(&breakdowns, PrettyConfig::new()).unwrap()
-    // );
 }
