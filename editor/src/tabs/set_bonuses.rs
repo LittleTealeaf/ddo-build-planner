3c647159a661c01e480682f5589427f8c54e565b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-17 12:22:10 -0500

    changes

--- editor/src/tabs/set_bonuses.rs
+++ editor/src/tabs/set_bonuses.rs
@@ -2,7 +2,10 @@ use builder::equipment::set_bonus::SetBonus;
 use fuzzy_filter::FuzzyFilter;
 use iced::{
     theme,
-    widget::{button, column, container, row, scrollable, text, text_input},
+    widget::{
+        button, column, container, horizontal_space, row, scrollable, text, text_input,
+        vertical_space,
+    },
     Alignment, Command, Length,
 };
 use iced_aw::{floating_element, graphics::icons::icon_to_char, Icon, ICON_FONT};
@@ -11,8 +14,14 @@ use crate::{Editor, EditorUpdate, EditorView, Message};
 
 #[derive(Clone, Debug, Default)]
 pub struct TabSetBonuses {
-    open_index: Option<usize>,
+    opened: Option<OpenedSet>,
     set_search_term: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct OpenedSet {
+    index: Option<usize>,
+    set: SetBonus,
 }
 
 #[derive(Debug, Clone)]
@@ -20,9 +29,10 @@ pub enum MessageSetBonuses {
     OpenSet(usize),
     DeleteSet(usize),
     CreateSet,
-    CloseSet,
+    SaveChanges,
+    CancelEdit,
     SearchSets(String),
-    SetSetName(usize, String),
+    EditSet(MessageEditSetBonus),
 }
 
 impl From<MessageSetBonuses> for Message {
@@ -31,19 +41,32 @@ impl From<MessageSetBonuses> for Message {
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum MessageEditSetBonus {
+    SetName(String),
+}
+
+impl From<MessageEditSetBonus> for Message {
+    fn from(value: MessageEditSetBonus) -> Self {
+        MessageSetBonuses::EditSet(value).into()
+    }
+}
+
 impl EditorUpdate<MessageSetBonuses> for Editor {
     fn handle_update(&mut self, message: MessageSetBonuses) -> iced::Command<Self::Message> {
         match &mut self.set_bonuses {
             None => Command::none(),
             Some(set_bonuses) => match message {
-                MessageSetBonuses::OpenSet(set) => {
-                    if set_bonuses.len() > set {
-                        self.tab_set_bonuses.open_index = Some(set);
-                    }
+                MessageSetBonuses::OpenSet(index) => {
+                    self.tab_set_bonuses.opened =
+                        set_bonuses.get(index).cloned().map(|set| OpenedSet {
+                            index: Some(index),
+                            set,
+                        });
                     Command::none()
                 }
-                MessageSetBonuses::CloseSet => {
-                    self.tab_set_bonuses.open_index = None;
+                MessageSetBonuses::CancelEdit => {
+                    self.tab_set_bonuses.opened = None;
                     Command::none()
                 }
                 MessageSetBonuses::DeleteSet(set) => {
@@ -53,20 +76,35 @@ impl EditorUpdate<MessageSetBonuses> for Editor {
                     Command::none()
                 }
                 MessageSetBonuses::CreateSet => {
-                    let index = set_bonuses.len();
-                    set_bonuses.push(SetBonus::new(format!("Set {}", &set_bonuses.len())));
-                    self.handle_update(MessageSetBonuses::OpenSet(index))
-                }
-                MessageSetBonuses::SetSetName(index, name) => {
-                    if let Some(set) = set_bonuses.get_mut(index) {
-                        set.set_name(name);
-                    }
+                    self.tab_set_bonuses.opened = Some(OpenedSet {
+                        index: None,
+                        set: SetBonus::new(String::new()),
+                    });
                     Command::none()
                 }
                 MessageSetBonuses::SearchSets(search) => {
                     self.tab_set_bonuses.set_search_term = search;
                     Command::none()
                 }
+                MessageSetBonuses::SaveChanges => {
+                    if let Some(opened) = &self.tab_set_bonuses.opened {
+                        if let Some(index) = opened.index {
+                            set_bonuses[index] = opened.set.clone();
+                        } else {
+                            set_bonuses.push(opened.set.clone());
+                        }
+                    }
+                    self.handle_update(MessageSetBonuses::CancelEdit)
+                }
+                MessageSetBonuses::EditSet(edit) => match &mut self.tab_set_bonuses.opened {
+                    None => Command::none(),
+                    Some(opened) => match edit {
+                        MessageEditSetBonus::SetName(name) => {
+                            opened.set.set_name(name);
+                            Command::none()
+                        }
+                    },
+                },
             },
         }
     }
@@ -86,7 +124,7 @@ impl EditorView<TabSetBonuses> for Editor {
                     .into()
             },
             |set_bonuses| {
-                self.tab_set_bonuses.open_index.map_or_else(
+                self.tab_set_bonuses.opened.as_ref().map_or_else(
                     || {
                         container(floating_element(
                             column!(
@@ -126,30 +164,30 @@ impl EditorView<TabSetBonuses> for Editor {
                             )
                             .width(Length::Fill)
                             .height(Length::Fill),
-                            button(text(icon_to_char(Icon::Plus)).font(ICON_FONT))
+                            button(text(icon_to_char(Icon::Plus)).font(ICON_FONT).size(35))
                                 .on_press(MessageSetBonuses::CreateSet.into()),
                         ))
                         .width(Length::Fill)
                         .height(Length::Fill)
                         .into()
                     },
-                    |index| {
-                        container(floating_element(
-                            column!(text_input("Name", set_bonuses.get(index).unwrap().name())
-                                .size(30)
-                                .on_input(move |i| MessageSetBonuses::SetSetName(index, i).into())),
-                            button(text(icon_to_char(Icon::Back)).font(ICON_FONT))
-                                .on_press(MessageSetBonuses::CloseSet.into())
-                                .style(theme::Button::Primary),
-                        ))
-                        .width(Length::Fill)
-                        .height(Length::Fill)
+                    |set| {
+                        column!(
+                            text_input("Set Name...", set.set.name())
+                                .on_input(|i| MessageEditSetBonus::SetName(i).into())
+                                .size(20),
+                            vertical_space(Length::Fill),
+                            row!(
+                                horizontal_space(Length::Fill),
+                                button(text("Cancel"))
+                                    .style(theme::Button::Secondary)
+                                    .on_press(MessageSetBonuses::CancelEdit.into()),
+                                button(text("Save"))
+                                    .style(theme::Button::Primary)
+                                    .on_press(MessageSetBonuses::SaveChanges.into())
+                            )
+                        )
                         .into()
-                        // column!(
-                        //     text(format!("Opened index {index}")),
-                        //     button("Back").on_press(MessageSetBonuses::CloseSet.into())
-                        // )
-                        // .into()
                     },
                 )
             },
