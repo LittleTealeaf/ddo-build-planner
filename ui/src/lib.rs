ea08c41447a82fbe7e2759cc10060088437540db
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-17 15:50:47 -0500

    added new ui library

---
+++ ui/src/lib.rs
@@ -0,0 +1,1 @@
+//! Iced ui code used in both the editor and the application
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -3,7 +3,5 @@
 pub mod bit_ops;
 pub mod float;
 pub mod hashmap;
-#[cfg(feature = "iced")]
-pub mod iced;
 pub mod modules;
 pub mod ord;
