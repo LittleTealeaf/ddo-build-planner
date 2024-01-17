48d2a0d9296535ae29dfa66d7f078ba0cc350796
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-17 09:22:28 -0500

    started set_bonuses layout

--- editor/src/tabs/set_bonuses.rs
+++ editor/src/tabs/set_bonuses.rs
@@ -1,18 +1,25 @@
-use iced::widget::text;
+use iced::{
+    widget::{container, text},
+    Length,
+};
 
-use crate::{EditorView, Editor};
-
-
+use crate::{Editor, EditorView};
 
 #[derive(Clone, Debug, Default)]
 pub struct TabSetBonuses {
-
+    open_index: Option<usize>,
 }
 
 impl EditorView<TabSetBonuses> for Editor {
     fn handle_view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        text("Set Bonuses").into()
+        match self.set_bonuses {
+            None => container(text("Loading Set Bonuses..."))
+                .height(Length::Fill)
+                .width(Length::Fill)
+                .center_x()
+                .center_y()
+                .into(),
+            Some(_) => text("Set Bonuses").into(),
+        }
     }
 }
-
-
