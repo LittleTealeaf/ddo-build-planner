458ad252c1c42ef86f53f42e765f03ae39528da7
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-23 14:58:50 -0500

    updated some tests / stuff in data

--- data/src/set_bonuses.rs
+++ data/src/set_bonuses.rs
@@ -1,29 +1,32 @@
 use builder::{breakdowns::Breakdowns, equipment::set_bonus::SetBonus};
-
-use crate::ParseError;
+use ron::error::SpannedError;
 
 /// Returns a list of set bonuses pulled from the data source
 ///
 /// # Errors
 /// Parse Errors (This inicates that there is a parsing error in the data.)
-pub fn get_set_bonuses() -> Result<Vec<SetBonus>, ParseError> {
+pub fn get_set_bonuses() -> Result<Vec<SetBonus>, SpannedError> {
     include_data!("set_bonuses")
 }
 
 /// A trait to implement the `.include_set_bonuses()` to [`Breakdowns`]
 pub trait IncludeSetBonuses {
     /// Inserts all set bonuses as dynamic bonuses
-    fn include_set_bonuses(&mut self);
+    ///
+    /// # Errors
+    /// Returns a Parsing error if parsing fails
+    fn include_set_bonuses(&mut self) -> Result<(), SpannedError>;
 }
 
 impl IncludeSetBonuses for Breakdowns {
-    fn include_set_bonuses(&mut self) {
+    fn include_set_bonuses(&mut self) -> Result<(), SpannedError> {
         self.import_dynamic_bonuses(
-            get_set_bonuses()
-                .unwrap()
+            get_set_bonuses()?
                 .into_iter()
                 .map(SetBonus::to_dynamic_bonus),
         );
+
+        Ok(())
     }
 }
 
@@ -39,6 +42,6 @@ mod tests {
     #[test]
     fn breakdowns_inserts_bonuses() {
         let mut breakdowns = Breakdowns::new();
-        breakdowns.include_set_bonuses();
+        assert!(breakdowns.include_set_bonuses().is_ok());
     }
 }
