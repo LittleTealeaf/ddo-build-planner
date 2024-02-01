44a5f61c14c906a28c754c8f59da7795181087c0
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-02-01 14:37:07 -0500

    update dynamic bonuses

--- builder/src/breakdowns/dynamic.rs
+++ builder/src/breakdowns/dynamic.rs
@@ -14,23 +14,31 @@ use super::Breakdowns;
 /// apply). This means that these bonuses will all show up in the breakdowns.
 impl Breakdowns {
     /// Shortcut for a single dynamic bonus. Uses [`Self::insert_dynamic_bonuses`]
-    pub fn import_dynamic_bonus(&mut self, attribute: Attribute, bonuses: Vec<BonusTemplate>) {
+    pub fn import_dynamic_bonus(
+        &mut self,
+        attribute: Attribute,
+        bonuses: impl IntoIterator<Item = BonusTemplate>,
+    ) {
         self.import_dynamic_bonuses([(attribute, bonuses)]);
     }
 
     /// Adds dynamic bonuses to the breakdowns. This is useful for attributes that need to be
     /// included automatically, but shouldn't always be there. Bonuses are only included if the
     /// given attribute is above 0.
-    pub fn import_dynamic_bonuses(
-        &mut self,
-        dynamic_bonuses: impl IntoIterator<Item = (Attribute, Vec<BonusTemplate>)>,
-    ) {
+    pub fn import_dynamic_bonuses<I, B>(&mut self, dynamic_bonuses: I)
+    where
+        I: IntoIterator<Item = (Attribute, B)>,
+        B: IntoIterator<Item = BonusTemplate>,
+    {
         let mut attributes = HashSet::new();
 
-        let dynamic_bonuses = dynamic_bonuses.into_iter().map(|bonus| {
-            attributes.insert(bonus.0.clone());
-            bonus
-        });
+        let dynamic_bonuses =
+            dynamic_bonuses
+                .into_iter()
+                .map(|(attribute, bonuses): (Attribute, B)| {
+                    attributes.insert(attribute.clone());
+                    (attribute, bonuses.into_iter().collect())
+                });
 
         self.dynamic_bonuses.extend(dynamic_bonuses);
 
