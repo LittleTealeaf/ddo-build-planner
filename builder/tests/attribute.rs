2d7c9f8e39ba13dde81ec089bba1b7f430023248
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-02-12 13:21:06 -0500

    added tests

---
+++ builder/tests/attribute.rs
@@ -0,0 +1,34 @@
+use builder::attribute::Attribute;
+use ron::{from_str, to_string};
+use utils::enums::StaticOptions;
+
+mod serialization {
+
+    use super::*;
+    #[test]
+    fn static_attributes_serialize() {
+        for attribute in Attribute::get_static() {
+            let serialized = to_string(&attribute).expect("Could not serialize attribute");
+            let deserialized: Attribute =
+                from_str(&serialized).expect("Could not deserialize attribute");
+
+            assert_eq!(
+                attribute, deserialized,
+                "Expected {attribute}, found {deserialized}"
+            );
+        }
+    }
+
+    #[test]
+    fn set_bonuses_serialize() {
+        let attribute = Attribute::SetBonus(String::from("Test"));
+        let serialized = to_string(&attribute).expect("Could not serialize attribute");
+        let deserialized: Attribute =
+            from_str(&serialized).expect("Could not deserialize attribute");
+
+        assert_eq!(
+            attribute, deserialized,
+            "Expected {attribute}, found {deserialized}"
+        );
+    }
+}
