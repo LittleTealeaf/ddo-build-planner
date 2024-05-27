171da29ed7b71ae8bc334db7ce1027a6f67fbaff
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-05-27 16:31:53 -0400

    added aliases

--- builder/src/types/toggle/guild_amenities.rs
+++ builder/src/types/toggle/guild_amenities.rs
@@ -9,124 +9,184 @@ use super::{ToToggle, Toggle};
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Hash, Clone, Copy)]
 pub enum GuildAmenity {
     /// Sign of the Silver Flame I
+    #[serde(rename = "sotsf1", alias = "SignOfTheSilverFlameI")]
     SignOfTheSilverFlameI,
     /// Shrine to the Devourer I
+    #[serde(rename = "sttd1", alias = "ShrineToTheDevourerI")]
     ShrineToTheDevourerI,
     /// Stormreaver Memorial I
+    #[serde(rename = "sm1", alias = "StormreaverMemorialI")]
     StormreaverMemorialI,
     /// Shrine of Experience I
+    #[serde(rename = "soe1", alias = "ShrineOfExperienceI")]
     ShrineOfExperienceI,
     /// The Orien Express
+    #[serde(rename = "toe", alias = "TheOrienExpress")]
     TheOrienExpress,
     /// Three-Finger Thad's
+    #[serde(rename = "tft", alias = "ThreeFingerThads")]
     ThreeFingerThads,
     /// Farshifter's Chambers
+    #[serde(rename = "fc", alias = "FarshiftersChambers")]
     FarshiftersChambers,
     /// Chronoscope
+    #[serde(rename = "c", alias = "Chronoscope")]
     Chronoscope,
     /// Sellsword's Tavern
+    #[serde(rename = "st", alias = "SellswordsTavern")]
     SellswordsTavern,
     /// Bath House
+    #[serde(rename = "bho", alias = "BathHouse")]
     BathHouse,
     /// Floating Rock Garden
+    #[serde(rename = "frg", alias = "FloatingRockGarden")]
     FloatingRockGarden,
     /// Paradoxical Puzzle Box
+    #[serde(rename = "ppb", alias = "ParadoxicalPuzzleBox")]
     ParadoxicalPuzzleBox,
     /// Old Sully's Grog Cellar
+    #[serde(rename = "osgc", alias = "OldSullysGrogCellar")]
     OldSullysGrogCellar,
     /// Throne Room
+    #[serde(rename = "tr", alias = "ThroneRoom")]
     ThroneRoom,
     /// Guild Storage I
+    #[serde(rename = "gs1", alias = "GuildStorageI")]
     GuildStorageI,
     /// Shrine of Experience II
+    #[serde(rename = "soe2", alias = "ShrineOfExperienceII")]
     ShrineOfExperienceII,
     /// Tactical Training Room
+    #[serde(rename = "ttr", alias = "TacticalTrainingRoom")]
     TacticalTrainingRoom,
     /// Danger Room
+    #[serde(rename = "dr", alias = "DangerRoom")]
     DangerRoom,
     /// Forbidden Library
+    #[serde(rename = "fl", alias = "ForbiddenLibrary")]
     ForbiddenLibrary,
     /// Archery Range
+    #[serde(rename = "ar", alias = "ArcheryRange")]
     ArcheryRange,
     /// Armory
+    #[serde(rename = "am", alias = "Armory")]
     Armory,
     /// Otto's Irresistable Dancehall
+    #[serde(rename = "oid", alias = "OttosIrresistableDancehall")]
     OttosIrresistableDancehall,
     /// Crusader's Chapel
+    #[serde(rename = "cc", alias = "CrusadersChapel")]
     CrusadersChapel,
     /// Arcane Sanctum
+    #[serde(rename = "as", alias = "ArcaneSanctum")]
     ArcaneSanctum,
     /// Trapsmith's Workshop
+    #[serde(rename = "tw", alias = "TrapsmithsWorkshop")]
     TrapsmithsWorkshop,
     /// Shrine of Experience III
+    #[serde(rename = "soe3", alias = "ShrineOfExperienceIII")]
     ShrineOfExperienceIII,
     /// Wild Grove
+    #[serde(rename = "wg", alias = "WildGrove")]
     WildGrove,
     /// Grandmaster's Dojo
+    #[serde(rename = "gd", alias = "GrandmastersDojo")]
     GrandmastersDojo,
     /// Proving Ground
+    #[serde(rename = "pg", alias = "ProvingGround")]
     ProvingGround,
     /// Collegium of the Twelve
+    #[serde(rename = "cott", alias = "CollegiumOfTheTwelve")]
     CollegiumOfTheTwelve,
     /// Bash the Breakables Cargo Bay
+    #[serde(rename = "btbcb", alias = "BashTheBreakablesCargoBay")]
     BashTheBreakablesCargoBay,
     /// Black Abbot's Shadow
+    #[serde(rename = "bas", alias = "BlackAbbotsShadow")]
     BlackAbbotsShadow,
     /// Banquet Hall
+    #[serde(rename = "bh", alias = "BanquetHall")]
     BanquetHall,
     /// Concert Hall
+    #[serde(rename = "ch", alias = "ConcertHall")]
     ConcertHall,
     /// Archwizard
+    #[serde(rename = "a", alias = "Archwizard")]
     Archwizard,
     /// Green Steel Crafting Hall
+    #[serde(rename = "gsch", alias = "GreenSteelCraftingHall")]
     GreenSteelCraftingHall,
     /// Shrine of Experience IV
+    #[serde(rename = "soh5", alias = "ShrineOfExperienceIV")]
     ShrineOfExperienceIV,
     /// Cannith Crafting Hall
+    #[serde(rename = "cch", alias = "CannithCraftingHall")]
     CannithCraftingHall,
     /// Game Hunter
+    #[serde(rename = "gm", alias = "GameHunter")]
     GameHunter,
     /// Fencing Master
+    #[serde(rename = "fm", alias = "FencingMaster")]
     FencingMaster,
     /// Ninja Assassin
+    #[serde(rename = "na", alias = "NinjaAssassin")]
     NinjaAssassin,
     /// Hag Apothecary
+    #[serde(rename = "ha", alias = "HagApothecary")]
     HagApothecary,
     /// Guild Storage II
+    #[serde(rename = "gs2", alias = "GuildStorageII")]
     GuildStorageII,
     /// Grand Reliquary I
+    #[serde(rename = "gr1", alias = "GrandReliquaryI")]
     GrandReliquaryI,
     /// Shrine of Experience V
+    #[serde(rename = "soe5", alias = "ShrineOfExperienceV")]
     ShrineOfExperienceV,
     /// Sign of the Silver Flame II
+    #[serde(rename = "sotsf2", alias = "SignOfTheSilverFlameII")]
     SignOfTheSilverFlameII,
     /// Shrine to the Devourer II
+    #[serde(rename = "sttd2", alias = "ShrineToTheDevourerII")]
     ShrineToTheDevourerII,
     /// Guild Storage III
+    #[serde(rename = "gs3", alias = "GuildStorageIII")]
     GuildStorageIII,
     /// Stormreaver Memorial II
+    #[serde(rename = "sm2", alias = "StormreaverMemorialII")]
     StormreaverMemorialII,
     /// Grand Reliquary II
+    #[serde(rename = "gr2", alias = "GrandReliquaryII")]
     GrandReliquaryII,
     /// Sign of the Silver Flame III
+    #[serde(rename = "sotsf3", alias = "SignOfTheSilverFlameIII")]
     SignOfTheSilverFlameIII,
     /// Shrine to the Devourer III
+    #[serde(rename = "sttd3", alias = "ShrineToTheDevourerIII")]
     ShrineToTheDevourerIII,
     /// Guild Storage IV
+    #[serde(rename = "gs4", alias = "GuildStorageIV")]
     GuildStorageIV,
     /// Stormreaver Memorial III
+    #[serde(rename = "sm3", alias = "StormreaverMemorialIII")]
     StormreaverMemorialIII,
     /// Grand Reliquary III
+    #[serde(rename = "gr3", alias = "GrandReliquaryIII")]
     GrandReliquaryIII,
     /// Guild Storage V
+    #[serde(rename = "gs5", alias = "GuildStorageV")]
     GuildStorageV,
     /// Sign of the Silver Flame IV
+    #[serde(rename = "sotsf4", alias = "SignOfTheSilverFlameIV")]
     SignOfTheSilverFlameIV,
     /// Shrine to the Devourer IV
+    #[serde(rename = "sttd4", alias = "ShrineToTheDevourerIV")]
     ShrineToTheDevourerIV,
     /// Stormreaver Memorial IV
+    #[serde(rename = "sm4", alias = "StormreaverMemorialIV")]
     StormreaverMemorialIV,
     /// Grand Reliquary IV
+    #[serde(rename = "gr4", alias = "GrandReliquaryIV")]
     GrandReliquaryIV,
 }
 
