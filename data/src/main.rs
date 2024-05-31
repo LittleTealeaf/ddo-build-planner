d41b11cc48eacb097e66413f03ec755dd5c15416
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-05-31 09:52:48 -0400

    use anyhow in data script

--- data/src/main.rs
+++ data/src/main.rs
@@ -1,36 +1,28 @@
 //! Loads and saves the data files to update their serialization
 
 use std::{
-    fs::{remove_file, File, OpenOptions},
+    fs::{File, OpenOptions},
     io::{BufReader, Write},
     path::Path,
 };
 
+use anyhow::Result;
 use builder::equipment::set_bonus::ItemSet;
 use ron::{de::from_reader, ser::to_string_pretty};
 use utils::ron::pretty_config::compact_pretty_config;
 
-fn main() {
-    update_item_sets();
-}
-
-fn update_item_sets() {
+fn main() -> Result<()> {
     let path = Path::new(".")
         .join("data")
         .join("data")
         .join("item_sets.ron");
 
-    let file = OpenOptions::new().read(true).open(path.clone()).unwrap();
+    let file = OpenOptions::new().read(true).open(path.clone())?;
+    let data: Vec<ItemSet> = from_reader(BufReader::new(file))?;
 
-    let data: Vec<ItemSet> = from_reader(BufReader::new(file)).unwrap();
+    let mut file = File::create(path)?;
 
-    remove_file(path.clone()).unwrap();
+    file.write_all(to_string_pretty(&data, compact_pretty_config())?.as_bytes())?;
 
-    let mut file = File::create(path).unwrap();
-    file.write_all(
-        to_string_pretty(&data, compact_pretty_config())
-            .unwrap()
-            .as_bytes(),
-    )
-    .unwrap();
+    Ok(())
 }
