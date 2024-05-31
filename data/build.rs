5b2810ef2a63d03912784e89b2500f93e702dcf6
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-05-31 09:39:55 -0400

    use anyhow

--- data/build.rs
+++ data/build.rs
@@ -4,15 +4,16 @@
 use std::{
     env,
     fs::{read_dir, File, ReadDir},
-    io::Write,
+    io::{BufReader, Write},
     path::Path,
 };
 
-use data::item_sets;
-use errors::Error;
+use anyhow::Result;
+use builder::equipment::set_bonus::ItemSet;
+use ron::de::from_reader;
 use serde::Serialize;
 
-fn main() -> Result<(), self::errors::Error> {
+fn main() -> Result<()> {
     write_artifact(
         "test",
         String::from("This is test data from the build script"),
@@ -23,7 +24,7 @@ fn main() -> Result<(), self::errors::Error> {
     Ok(())
 }
 
-fn write_artifact<S>(name: &str, item: S) -> Result<(), Error>
+fn write_artifact<S>(name: &str, item: S) -> Result<()>
 where
     S: Serialize,
 {
@@ -36,7 +37,7 @@ where
     Ok(())
 }
 
-fn get_data_files(dir: &str) -> Result<ReadDir, Error> {
+fn get_data_files(dir: &str) -> Result<ReadDir> {
     let path = Path::new(".").join("data").join(dir);
     let path_str = path.to_str().unwrap();
 
@@ -45,58 +46,11 @@ fn get_data_files(dir: &str) -> Result<ReadDir, Error> {
     Ok(read_dir(path)?)
 }
 
-mod data {
-    use std::{fs::File, io::BufReader, path::Path};
-
-    use builder::equipment::set_bonus::ItemSet;
-    use ron::de::from_reader;
-
-    use crate::errors;
-
-    pub fn item_sets() -> Result<Vec<ItemSet>, errors::Error> {
-        println!("cargo:rerun-if-changed=./data/item_sets.ron");
-        let path = Path::new("./data/item_sets.ron");
-        let file = File::open(path)?;
-        let reader = BufReader::new(file);
-        let items = from_reader(reader)?;
-        Ok(items)
-    }
-}
-
-mod errors {
-    use std::{env::VarError, io};
-
-    use ron::de::SpannedError;
-
-    #[derive(Debug)]
-    pub enum Error {
-        Environment(VarError),
-        Serialize(ron::Error),
-        Spanned(SpannedError),
-        IO(io::Error),
-    }
-
-    impl From<VarError> for Error {
-        fn from(value: VarError) -> Self {
-            Self::Environment(value)
-        }
-    }
-
-    impl From<ron::Error> for Error {
-        fn from(value: ron::Error) -> Self {
-            Self::Serialize(value)
-        }
-    }
-
-    impl From<io::Error> for Error {
-        fn from(value: io::Error) -> Self {
-            Self::IO(value)
-        }
-    }
-
-    impl From<SpannedError> for Error {
-        fn from(value: SpannedError) -> Self {
-            Self::Spanned(value)
-        }
-    }
+fn item_sets() -> Result<Vec<ItemSet>> {
+    println!("cargo:rerun-if-changed=./data/item_sets.ron");
+    let path = Path::new("./data/item_sets.ron");
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let items = from_reader(reader)?;
+    Ok(items)
 }
