55cf814c05f7c4508d95bceae24f31686555c5d0
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-06-21 13:22:45 -0400

    changes, need to do documentation

--- builder/src/breakdowns/breakdown.rs
+++ builder/src/breakdowns/breakdown.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
-
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
+use utils::{from_into::FromInto, hashmap::IntoGroupedHashMap};
 
 use crate::{
     attribute::Attribute,
@@ -10,67 +9,77 @@ use crate::{
 
 use super::Breakdowns;
 
-/// Provides both the [`Bonus`] as well as the calculated [`Value`].
-///
-/// [`Value`]: crate::bonus::Value
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
-pub struct BonusEntry {
-    bonus: Bonus,
+/// Provides the value breakdown of a particular attribute
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
+pub struct AttributeBreakdown {
+    stacking: Vec<BonusEntry>,
+    disabled_stacking: Vec<BonusEntry>,
+    bonuses: Vec<BonusTypeEntry>,
     value: Decimal,
 }
 
-impl BonusEntry {
-    /// Returns the [`Bonus`] object
-    #[must_use]
-    pub const fn bonus(&self) -> &Bonus {
-        &self.bonus
+impl AttributeBreakdown {
+    pub const fn stacking(&self) -> &Vec<BonusEntry> {
+        &self.stacking
     }
 
-    /// Returns the calculated [`Value`] as a [`Decimal`]
-    ///
-    /// [`Value`]: crate::bonus::Value
-    #[must_use]
+    pub const fn disabled_stacking(&self) -> &Vec<BonusEntry> {
+        &self.disabled_stacking
+    }
+
+    pub const fn bonuses(&self) -> &Vec<BonusTypeEntry> {
+        &self.bonuses
+    }
+
     pub const fn value(&self) -> &Decimal {
         &self.value
     }
 }
 
-/// Provides a complete breakdown of an attribute
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
-pub struct AttributeBreakdown {
-    applied: Vec<BonusEntry>,
+/// Provides the details regarding the breakdown of a particular bonus type
+#[derive(PartialEq, Eq, Clone, Hash, Debug, Serialize, Deserialize)]
+pub struct BonusTypeEntry {
+    bonus_type: BonusType,
+    applied: Option<BonusEntry>,
     overwritten: Vec<BonusEntry>,
     disabled: Vec<BonusEntry>,
-    value: Decimal,
 }
 
-impl AttributeBreakdown {
-    /// Returns the active and applied [`BonusEntry`] items.
-    /// Calculating the sum of the values of these items will return the total value of the
-    /// attribute.
-    #[must_use]
-    pub const fn applied(&self) -> &Vec<BonusEntry> {
+impl BonusTypeEntry {
+    pub const fn bonus_type(&self) -> BonusType {
+        self.bonus_type
+    }
+
+    pub const fn applied(&self) -> &Option<BonusEntry> {
         &self.applied
     }
 
-    /// Returns the overwritten [`BonusEntry`] items. Overwritten bonuses happen when there is a
-    /// larger bonus of the same [`BonusType`] in the [`Breakdowns`].
-    #[must_use]
     pub const fn overwritten(&self) -> &Vec<BonusEntry> {
         &self.overwritten
     }
 
-    /// Returns disabled [`BonusEntry`] items. Disabled bonuses are bonuses whose [`Condition`]
-    /// evaluates to false.
-    ///
-    /// [`Condition`]: crate::bonus::Condition
-    #[must_use]
     pub const fn disabled(&self) -> &Vec<BonusEntry> {
         &self.disabled
     }
+}
 
-    /// Returns the snapshotted / calculated value of the attribute.
-    #[must_use]
+/// Provides the details regarding the breakdown of a particular bonus
+#[derive(PartialEq, Eq, Clone, Hash, Debug, Serialize, Deserialize)]
+pub struct BonusEntry {
+    bonus: Bonus,
+    condition: bool,
+    value: Decimal,
+}
+
+impl BonusEntry {
+    pub const fn bonus(&self) -> &Bonus {
+        &self.bonus
+    }
+
+    pub const fn condition(&self) -> bool {
+        self.condition
+    }
+
     pub const fn value(&self) -> &Decimal {
         &self.value
     }
@@ -82,9 +91,13 @@ impl Breakdowns {
     /// [`get_breakdown()`]. This cached version is updated whenever a change is made.
     ///
     /// [`get_breakdown()`]: crate::breakdowns::Breakdowns::get_breakdown
-    pub fn track_breakdown(&mut self, attribute: Attribute) {
-        let breakdowns = self.build_breakdowns(&attribute);
-        self.cache.breakdowns.insert(attribute, breakdowns);
+    pub fn track_breakdown<A>(&mut self, attribute: A)
+    where
+        A: Into<Attribute>,
+    {
+        let attribute = Attribute::from_into(attribute);
+        let breakdown = self.build_breakdown(&attribute);
+        self.cache.breakdowns.insert(attribute, breakdown);
     }
 
     /// Returns a reference to the breakdown if it exists for the given attribute.
@@ -109,50 +122,61 @@ impl Breakdowns {
         self.cache.breakdowns.remove(attribute)
     }
 
-    pub(super) fn build_breakdowns(&mut self, attribute: &Attribute) -> AttributeBreakdown {
+    pub(super) fn build_breakdown(&mut self, attribute: &Attribute) -> AttributeBreakdown {
         let value = self.evaluate_attribute(attribute);
 
-        let mut breakdown = AttributeBreakdown {
-            applied: Vec::new(),
-            overwritten: Vec::new(),
-            disabled: Vec::new(),
-            value,
-        };
+        let mut attribute_bonuses = self
+            .bonuses
+            .get(attribute)
+            .cloned()
+            .into_iter()
+            .flatten()
+            .map(|bonus| {
+                (
+                    *bonus.bonus_type(),
+                    BonusEntry {
+                        value: self.evaluate_value(bonus.value()),
+                        condition: self.evaluate_condition(bonus.condition()),
+                        bonus,
+                    },
+                )
+            })
+            .into_grouped_hash_map();
 
-        let mut applied: HashMap<BonusType, BonusEntry> = HashMap::new();
+        let (stacking, disabled_stacking) = attribute_bonuses
+            .remove(&BonusType::Stacking)
+            .unwrap_or_default()
+            .into_iter()
+            .partition(|bonus| bonus.condition);
 
-        let bonuses = self.bonuses.get(attribute).cloned().unwrap_or_default();
+        let bonuses = attribute_bonuses
+            .into_iter()
+            .map(|(bonus_type, bonuses)| {
+                let (mut bonuses, disabled): (Vec<_>, Vec<_>) =
+                    bonuses.into_iter().partition(|bonus| bonus.condition);
 
-        for bonus in bonuses {
-            let value = self.evaluate_value(bonus.value());
-            let condition = self.evaluate_condition(bonus.condition());
+                let max = bonuses
+                    .iter()
+                    .enumerate()
+                    .max_by_key(|(_, bonus)| bonus.value)
+                    .map(|(index, _)| index);
 
-            let entry = BonusEntry { bonus, value };
+                let applied = max.map(|index| bonuses.swap_remove(index));
 
-            if condition {
-                match entry.bonus.bonus_type() {
-                    BonusType::Stacking => breakdown.applied.push(entry),
-                    bonus_type => {
-                        if let Some(existing) = applied.remove(bonus_type) {
-                            if existing.value >= value {
-                                applied.insert(*bonus_type, existing);
-                                breakdown.overwritten.push(entry);
-                            } else {
-                                applied.insert(*bonus_type, entry);
-                                breakdown.overwritten.push(existing);
-                            }
-                        } else {
-                            applied.insert(*bonus_type, entry);
-                        }
-                    }
+                BonusTypeEntry {
+                    overwritten: bonuses,
+                    bonus_type,
+                    applied,
+                    disabled,
                 }
-            } else {
-                breakdown.disabled.push(entry);
-            }
+            })
+            .collect();
+
+        AttributeBreakdown {
+            stacking,
+            disabled_stacking,
+            bonuses,
+            value,
         }
-
-        breakdown.applied.extend(applied.into_values());
-
-        breakdown
     }
 }
