016e13bea2ad3ae7f6fe8ad24178ceb3e1e6ff69
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-07-03 10:21:36 -0400

    changes

--- utils/src/maps/map_type.rs
+++ utils/src/maps/map_type.rs
@@ -6,9 +6,13 @@ use im::OrdMap;
 /// Defines some type as being map-like. This allows for each of the methods to work for any types
 /// that implement this trait
 pub trait MapLike<K, V> {
+    /// Returns `true` if the key has an associated value in the map, `false` otherwise
     fn contains_key(&self, key: &K) -> bool;
+    /// Inserts the key and value pair. Returns the overwritten value if there is one
     fn insert(&mut self, key: K, value: V) -> Option<V>;
+    /// Gets the associated value for a given key
     fn get(&self, key: &K) -> Option<&V>;
+    /// Gets a mutable reference to the value for a given key
     fn get_mut(&mut self, key: &K) -> Option<&mut V>;
 }
 
