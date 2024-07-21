deef851c95bfbde1d20be17583329a0d005768f5
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2024-07-21 14:05:07 -0400

    feat: Multiple Attribute Selector (#547)
    * changes
    
    * changes
    
    * Revert "changes"
    
    This reverts commit 7e8b32f75f13b66409545949ce940ae4a9a3fc3d.
    
    * selector?
    
    * implement tracked attributes in sandbox
    
    * fix changes
    
    * cleanup
    
    * cleaned up stuff
    
    * added additional functionality
    
    * more cleanups
    
    * remove todo
    
    * cleanup
    
    * changes

--- editor/src/modals/attribute.rs
+++ editor/src/modals/attribute.rs
@@ -1,4 +1,5 @@
-use std::string::ToString;
+use core::fmt;
+use std::{error::Error, string::ToString};
 
 use builder::attribute::Attribute;
 use fuzzy_filter::matches;
@@ -7,6 +8,7 @@ use iced::{
     widget::{button, column, container, row, scrollable, text, text_input},
     Application, Command, Element, Length, Renderer,
 };
+use im::OrdSet;
 use ui::{error, HandleMessage, HandleView};
 use utils::from_into::FromInto;
 
@@ -15,14 +17,13 @@ use crate::{App, Message};
 #[derive(Clone, Debug)]
 pub struct ModalAttribute {
     attributes: Vec<Attribute>,
-    selected: Option<usize>,
+    selected: OrdSet<usize>,
+    multiselect: bool,
     title: Option<String>,
     filter: String,
     on_submit: Option<Message>,
     on_cancel: Option<Message>,
 }
-
-// TODO: Filter Strings on Back-End
 
 impl App {
     pub fn select_attribute(&self) -> ModalAttribute {
@@ -30,6 +31,28 @@ impl App {
     }
 }
 
+#[derive(Clone, Debug)]
+pub enum ModalAttributeError {
+    InvalidIndex(usize),
+    NotMultiselected,
+    NotSingle,
+    NoSelection,
+}
+
+impl fmt::Display for ModalAttributeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidIndex(index) => write!(f, "Invalid Attribute Index: {index}"),
+            Self::NotMultiselected => write!(f, "Selector is not multiselector"),
+            Self::NotSingle => write!(f, "Selector is not single selector"),
+            Self::NoSelection => write!(f, "No Attribute Selected"),
+        }
+    }
+}
+
+impl Error for ModalAttributeError {}
+
+/// Constructor Methods
 impl ModalAttribute {
     fn new<I>(attributes: I) -> Self
     where
@@ -40,26 +63,30 @@ impl ModalAttribute {
 
         Self {
             attributes,
-            selected: None,
+            selected: OrdSet::new(),
             title: None,
+            multiselect: false,
             filter: String::new(),
             on_submit: None,
             on_cancel: None,
         }
     }
 
-    pub fn select<A>(self, attribute: A) -> Self
+    pub fn select<A>(mut self, attribute: A) -> Self
     where
         A: Into<Attribute>,
     {
         let attribute = Attribute::from_into(attribute);
-        Self {
-            selected: self
-                .attributes
-                .iter()
-                .enumerate()
-                .find_map(|(index, a)| a.eq(&attribute).then_some(index)),
-            ..self
+        if self.multiselect {
+            if let Some(index) = self.lookup(&attribute) {
+                self.selected.insert(index);
+            }
+            self
+        } else {
+            Self {
+                selected: self.lookup(&attribute).into_iter().collect(),
+                ..self
+            }
         }
     }
 
@@ -67,14 +94,39 @@ impl ModalAttribute {
     where
         A: Into<Attribute>,
     {
-        let attribute = attribute.map(|attribute| Attribute::from_into(attribute));
+        if let Some(attribute) = attribute {
+            self.select(attribute)
+        } else {
+            self
+        }
+    }
+
+    pub fn multiselect(self, enabled: bool) -> Self {
         Self {
-            selected: attribute.and_then(|attr| {
-                self.attributes
-                    .iter()
-                    .enumerate()
-                    .find_map(|(index, a)| a.eq(&attr).then_some(index))
-            }),
+            multiselect: enabled,
+            ..self
+        }
+    }
+
+    pub fn select_all<I, A>(mut self, attributes: I) -> Self
+    where
+        I: IntoIterator<Item = A>,
+        A: Into<Attribute>,
+    {
+        let indexes = attributes
+            .into_iter()
+            .map(Into::into)
+            .filter_map(|a| self.lookup(&a))
+            .collect::<Vec<_>>();
+
+        self.selected.extend(indexes);
+
+        self
+    }
+
+    pub fn clear_selected(self) -> Self {
+        Self {
+            selected: OrdSet::new(),
             ..self
         }
     }
@@ -85,6 +137,24 @@ impl ModalAttribute {
     {
         Self {
             title: Some(title.into()),
+            ..self
+        }
+    }
+
+    pub fn title_maybe<S>(self, title: Option<S>) -> Self
+    where
+        S: Into<String>,
+    {
+        if let Some(title) = title {
+            self.title(title)
+        } else {
+            self
+        }
+    }
+
+    pub fn clear_title(self) -> Self {
+        Self {
+            title: None,
             ..self
         }
     }
@@ -103,8 +173,16 @@ impl ModalAttribute {
     where
         M: Into<Message>,
     {
+        if let Some(message) = message {
+            self.on_submit(message)
+        } else {
+            self
+        }
+    }
+
+    pub fn clear_on_submit(self) -> Self {
         Self {
-            on_submit: message.map(Into::into),
+            on_submit: None,
             ..self
         }
     }
@@ -123,14 +201,60 @@ impl ModalAttribute {
     where
         M: Into<Message>,
     {
-        Self {
-            on_cancel: message.map(Into::into),
-            ..self
+        if let Some(message) = message {
+            self.on_cancel(message)
+        } else {
+            self
         }
     }
 
-    pub fn get_attribute(&self) -> Option<Attribute> {
-        self.attributes.get(self.selected?).cloned()
+    pub fn clear_on_cancel(self) -> Self {
+        Self {
+            on_cancel: None,
+            ..self
+        }
+    }
+}
+
+/// Methods for Accessing Information
+impl ModalAttribute {
+    pub fn get_attribute(&self) -> Result<Attribute, ModalAttributeError> {
+        if self.multiselect {
+            return Err(ModalAttributeError::NotSingle);
+        }
+
+        let Some(index) = self.selected.get_min() else {
+            return Err(ModalAttributeError::NoSelection);
+        };
+
+        let Some(attribute) = self.attributes.get(*index) else {
+            return Err(ModalAttributeError::InvalidIndex(*index));
+        };
+
+        Ok(attribute.clone())
+    }
+
+    pub fn get_attributes(
+        &self,
+    ) -> Result<impl Iterator<Item = Attribute> + '_, ModalAttributeError> {
+        if !self.multiselect {
+            return Err(ModalAttributeError::NotMultiselected);
+        };
+
+        Ok(self
+            .selected
+            .iter()
+            .filter_map(|index| self.attributes.get(*index))
+            .cloned())
+    }
+}
+
+impl ModalAttribute {
+    fn lookup(&self, attribute: &Attribute) -> Option<usize> {
+        self.attributes
+            .iter()
+            .enumerate()
+            .find_map(|(index, a)| a.eq(attribute).then_some(index))
     }
 }
 
@@ -164,7 +288,16 @@ impl HandleMessage<ModalAttributeMessage> for App {
                 Command::none()
             }
             ModalAttributeMessage::Select(index) => {
-                sel.selected = Some(index);
+                if !sel.multiselect {
+                    sel.selected.clear();
+                }
+
+                if sel.selected.contains(&index) {
+                    sel.selected.remove(&index);
+                } else {
+                    sel.selected.insert(index);
+                }
+
                 Command::none()
             }
             ModalAttributeMessage::Submit => {
@@ -184,7 +317,7 @@ impl HandleMessage<ModalAttributeMessage> for App {
                 command
             }
             ModalAttributeMessage::Clear => {
-                sel.selected = None;
+                sel.selected.clear();
                 Command::none()
             }
         }
@@ -197,7 +330,6 @@ impl HandleView<App> for ModalAttribute {
         _app: &'a App,
     ) -> Element<'_, <App as Application>::Message, <App as Application>::Theme, Renderer> {
         let filter = self.filter.to_lowercase();
-        let selected = self.selected.unwrap_or(self.attributes.len());
 
         column!(
             row!(
@@ -210,22 +342,21 @@ impl HandleView<App> for ModalAttribute {
                 button(text("Submit"))
                     .style(theme::Button::Primary)
                     .on_press_maybe(
-                        self.selected
-                            .is_some()
+                        (!self.selected.is_empty() || self.multiselect)
                             .then_some(ModalAttributeMessage::Submit.into())
                     )
             ),
             scrollable(column(
                 self.attributes
                     .iter()
+                    .map(ToString::to_string)
                     .enumerate()
-                    .map(|(index, attribute)| (index, attribute.to_string()))
                     .filter(|(_, str)| matches(&filter, str.to_lowercase().as_ref()))
                     .map(|(index, attr)| {
                         container(
                             button(text(attr))
                                 .on_press(ModalAttributeMessage::Select(index).into())
-                                .style(if selected == index {
+                                .style(if self.selected.contains(&index) {
                                     theme::Button::Primary
                                 } else {
                                     theme::Button::Text
