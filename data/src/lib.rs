0ddd907766c3a0c590ef90b1a79f0f53e92121f5
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-08-12 16:26:11 -0400

    add warning

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -3,6 +3,10 @@ use ddo_core::items::feat::Feat;
 #[macro_export]
 macro_rules! load_data {
     ($function: ident, $type: ty, $file: expr) => {
+        /// Loads Data that has been Serialized in the binary
+        ///
+        /// # Errors
+        /// Returns an error if there is a parsing issue from the data
         pub fn $function() -> Result<$type, ron::de::SpannedError> {
             ron::from_str(include_str!(concat!(env!("OUT_DIR"), "/", $file)))
         }
